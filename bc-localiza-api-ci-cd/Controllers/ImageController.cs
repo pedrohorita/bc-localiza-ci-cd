bd76e079f78b0e5dec7a764b272f0c7943916681
Pedro R T A Horita <pedro_horita@hotmail.com>
2022-01-05 20:44:59 -0300

    Criação API de Images
    Criação de uma API que retorna imagens armazenadas na internet.

---
+++ bc-localiza-api-ci-cd/Controllers/ImageController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace bc_localiza_api_ci_cd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        
+        private readonly ILogger<ImageController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public ImageController(ILogger<ImageController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get(bool image)
+        {
+            var random = new Random();
+            var url = _configuration.GetValue<string>("URLBase") + random.Next(1, 10) + ".jpg";
+            if (image)
+            {
+                using var webClient = new WebClient();
+                byte[] imageBytes = webClient.DownloadData(url);
+                return File(imageBytes, "image/jpg");
+            }
+            else
+            {
+                return Ok(url);
+            }
+        }
+    }
+}
